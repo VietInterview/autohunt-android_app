839ef457f72f936b47c5863b6ff726d2c94aa68f
Nguyen Nghia Hiep <hiep.nguyen@vietinterview.com>
2018-10-13 23:11:25 +0700

    test branch master switch dev

--- app/src/main/java/com/vietinterview/getbee/AccountManager.java
+++ app/src/main/java/com/vietinterview/getbee/AccountManager.java
@@ -76,7 +76,7 @@ public class AccountManager {
 
     public static String getUDID() {
         if (UDID == null) {
-            //Test git master
+            //Test git master switch dev
             UDID = Settings.Secure.getString(GetBeeApplication.getInstance().getContentResolver(),
                     Settings.Secure.ANDROID_ID);
         }
