96a58654367ad4c0179ab4dc2ccf1372f6c15873
Nguyen Nghia Hiep <hiep.nguyen@vietinterview.com>
2018-10-13 22:06:05 +0700

    test git 1

--- app/src/main/java/com/vietinterview/getbee/AccountManager.java
+++ app/src/main/java/com/vietinterview/getbee/AccountManager.java
@@ -76,7 +76,7 @@ public class AccountManager {
 
     public static String getUDID() {
         if (UDID == null) {
-            //Test git
+            //Test git 1
             UDID = Settings.Secure.getString(GetBeeApplication.getInstance().getContentResolver(),
                     Settings.Secure.ANDROID_ID);
         }
