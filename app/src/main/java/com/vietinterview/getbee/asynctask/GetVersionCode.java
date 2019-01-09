9e9dc2350b6d7c45d2197ccee847b00d8b54cb7c
Nguyen Nghia Hiep <hiep.nguyen@vietinterview.com>
2019-01-09 15:20:59 +0700

    fix bug va update

---
+++ app/src/main/java/com/vietinterview/getbee/asynctask/GetVersionCode.java
@@ -0,0 +1,8 @@
+package com.vietinterview.getbee.asynctask;
+
+/**
+ * Created by hiepnguyennghia on 1/9/19.
+ * Copyright © 2018 Vietinterview. All rights reserved.
+ */
+public class GetVersionCode {
+}
